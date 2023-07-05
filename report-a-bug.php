379e5b3ca09b42020379ec8cfa8d2e8a42eee494
Andy Fragen <andy@thefragens.com>
2023-07-05 05:03:02 -0700

    stubbing
    oops

---
+++ report-a-bug.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Report a Bug
+ *
+ * @package Report_A_Bug
+ * @author Andy Fragen, Colin Stewart.
+ * @license MIT
+ */
+
+/**
+ * Plugin Name:       WordPress Beta Tester
+ * Plugin URI:        https://wordpress.org/plugins/wordpress-beta-tester/
+ * Description:       Provide easily accessible data for bug reporting.
+ * Author:            WordPress Upgrade/Install Team
+ * Version:           0.1.0
+ * Network:           true
+ * Author URI:        https://make.wordpress.org/core/components/upgrade-install/
+ * Text Domain:       report-a-bug
+ * Domain Path:       /languages
+ * License:           MIT
+ * License URI:       https://www.opensource.org/licenses/MIT
+ * GitHub Plugin URI: https://github.com/afragen/report-a-bug
+ * Primary Branch:    main
+ * Requires at least: 5.9
+ * Requires PHP:      7.2
+ */
